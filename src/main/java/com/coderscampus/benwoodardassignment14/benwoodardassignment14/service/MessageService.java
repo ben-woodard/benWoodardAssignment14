1d00a1360c003913cf42aede11b9a5d08a3a3954
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-02-29 14:31:55 -0500

    updated messageService

--- src/main/java/com/coderscampus/benwoodardassignment14/benwoodardassignment14/service/MessageService.java
+++ src/main/java/com/coderscampus/benwoodardassignment14/benwoodardassignment14/service/MessageService.java
@@ -1,4 +1,13 @@
 package com.coderscampus.benwoodardassignment14.benwoodardassignment14.service;
 
+import com.coderscampus.benwoodardassignment14.benwoodardassignment14.domain.Message;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
+
+
+    public Message save(Message message) {
+        
+    }
 }
--- src/main/java/com/coderscampus/benwoodardassignment14/benwoodardassignment14/web/MessageController.java
+++ src/main/java/com/coderscampus/benwoodardassignment14/benwoodardassignment14/web/MessageController.java
@@ -1,4 +1,36 @@
 package com.coderscampus.benwoodardassignment14.benwoodardassignment14.web;
 
+import com.coderscampus.benwoodardassignment14.benwoodardassignment14.domain.Message;
+import com.coderscampus.benwoodardassignment14.benwoodardassignment14.service.ChannelService;
+import com.coderscampus.benwoodardassignment14.benwoodardassignment14.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class MessageController {
+
+    private final MessageService messageService;
+    private final ChannelService channelService;
+
+    public MessageController(MessageService messageService, ChannelService channelService) {
+        this.messageService = messageService;
+        this.channelService = channelService;
+    }
+
+    @PostMapping("message/create")
+    public ResponseEntity createMessage(@RequestBody Message message) {
+        System.out.println(message);
+        messageService.save(message);
+        if(message != null) {
+            return new ResponseEntity(message, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("Error in saving the message", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 }
